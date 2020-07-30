0768f5d99dc4f872c39c7640c08d90a66c83fa03
Hannah Bregman <bregmanhannah@gmail.com>
2020-07-30 14:40:51 -0400

    finalized features and readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
+  def authenticate_with_credentials(email, password)
+    #lower_case_email = email.downcase.strip
+    user = user.find_by_email(email)
+    user if user&.authenticate(password)
+  end
+
 end
